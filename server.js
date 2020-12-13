e9ab847d15e1b819b266efe25925b83f74b287e6
Jose <jmejiamu@gmail.com>
2020-12-12 20:48:37 -0800

    added update profile picture end point

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const knex = require('knex');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const bcrypt = require('bcrypt');
+const multer = require('multer');
 
 // Import all endpoints
 const getAllpost = require('./routes/gets');
@@ -16,8 +17,8 @@ const register = require('./controllers/register');
 const signin = require('./controllers/signin');
 
 //middleware
-const validinfo = require('./middlewares/validinfo')
-const authorization = require('./middlewares/authorization')
+const validinfo = require('./middlewares/validinfo');
+const authorization = require('./middlewares/authorization');
 
 dotenv.config();
 
@@ -36,6 +37,35 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
+//Set the destination and file name
+let storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, '/var/www/html/profile')
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.originalname)
+    }
+})
+
+// Initialization the multer  and set up the folder which store the image
+let upload = multer({ storage: storage });
+
+// update profile picture
+
+app.put('/updateprofilepicture/:user_id', upload.single('photo'), async (req, res) => {
+    try {
+        let path = 'http://157.245.229.180/profile/' + req.file.originalname
+        const { user_id } = req.params;
+
+        const updateProfile = await db('users').update({
+            picture: path
+        }).where({ id: user_id })
+        res.status(200).send({ response: 'Update profile picture' })
+    } catch (error) {
+        console.error(error.message);
+    }
+})
+
 app.get('/allpost', (req, res) => {
     getAllpost.showAllBlogs(req, res, db);
 });
@@ -52,6 +82,7 @@ app.delete('/deleteblog/:post_id', (req, res) => {
     deletePost.deleteblog(req, res, db);
 })
 
+
 app.post('/register', validinfo, async (req, res) => {
     const { email } = req.body;
 
@@ -65,7 +96,7 @@ app.post('/register', validinfo, async (req, res) => {
 
     } catch (error) {
         console.error(error.message);
-        res.status(400).json({ response: "Error" })
+        res.status(400).json({ response: "error" })
 
     }
 })
